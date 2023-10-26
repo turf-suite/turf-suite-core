25ef3f566cac4221b786bdd1c29a35c5ebaa269c
Ryan Monahan <ryanaldo34@gmail.com>
2023-10-26 18:02:04 -0500

    stik projects and supabase init

--- apps/stik/app/page.tsx
+++ apps/stik/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome hub 👋
+              Welcome stik 👋
             </h1>
           </div>
 
