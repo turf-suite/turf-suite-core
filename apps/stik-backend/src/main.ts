b9076bb905c2773f5e662348d00902514d4828de
Ryan Monahan <ryanaldo34@gmail.com>
2023-10-28 16:53:07 -0500

    Add swagger to stik backend

--- apps/stik-backend/src/main.ts
+++ apps/stik-backend/src/main.ts
@@ -5,13 +5,21 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Stik Backend')
+    .setDescription('The backend APIs for Stik')
+    .setVersion('pre-alpha')
+    .build();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, document);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
