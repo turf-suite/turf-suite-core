35ab8cba3943e932b25351d11f1eca5222e6b832
Ryan Monahan <ryanaldo34@gmail.com>
2023-11-11 14:14:18 -0600

    Supabase snapshot scripts

---
+++ libs/supabase/src/create_prod_snapshot.ts
@@ -0,0 +1,20 @@
+createSnapshotOfProd();
+
+async function createSnapshotOfProd() {
+  // eslint-disable-next-line
+  const { execSync } = require('child_process');
+
+  console.log('========== Create Snapshot ==========');
+
+  const now = new Date();
+  const inquirer = await import('inquirer');
+  const { name } =
+    (await (inquirer as any).default.prompt({
+      message: 'enter a name for the snapshot',
+      name: 'name',
+    })) || now.toISOString();
+
+  execSync(
+    `PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $POSTGRES_HOST -p 5432 -U postgres -a --inserts -f libs/supabase/supabase/snapshots/prod-${name}.sql`
+  );
+}
