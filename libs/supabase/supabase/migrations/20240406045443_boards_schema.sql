477130f6125834c25e558a2ea8ade47f06b4b0bd
Ryan Monahan <ryanaldo34@gmail.com>
2024-04-06 00:46:53 -0500

    add task transitions table

--- libs/supabase/supabase/migrations/20240406045443_boards_schema.sql
+++ libs/supabase/supabase/migrations/20240406045443_boards_schema.sql
@@ -8,11 +8,23 @@ create table if not exists public.boards (
 ) tablespace pg_default;
 
 create table if not exists public.board_states (
+  id big serial
   board_id big serial not null,
   name text not null,
   ordering number not null,
   is_completion_state boolean not null,
   is_cancelled_state boolean not null,
   constraint board_fkey foreign key (board_id) references public.boards (id) on delete cascade,
-  constraint board_states_pkey primary key (board_id, name)
+  constraint board_states_pkey primary key (id)
+) tablespace pg_default;
+
+create table if not exists public.board_task_state_transitions (
+  board_id big serial not null,
+  from_state_id big serial not null,
+  to_state_id big serial not null,
+  constraint task_state_transitions_pkey primary key (board_id, from_state_name, to_state_name),
+  constraint board_fkey foreign key (board_id) references public.boards (id),
+  constraint from_fkey foreign key (from_state_id) references public.board_states (id),
+  constraint to_fkey foreign key (to_state_id) references public.board_states (id),
+  constraint different_states check (from_state_id <> to_state_id)
 ) tablespace pg_default;
