cdfa6141aff3134a8585d4191c4da78f51109307
Ryan Monahan <ryanaldo34@gmail.com>
2024-04-05 23:40:33 -0500

    fix sql errors

--- libs/supabase/supabase/migrations/20240306015951_projects_schema.sql
+++ libs/supabase/supabase/migrations/20240306015951_projects_schema.sql
@@ -12,7 +12,7 @@ create table if not exists public.team_members (
     member_id uuid not null,
     org_id uuid not null,
     team_id serial not null,
-    constraint member_id_fkey foreign key (member_id) references public.profiles (id) on delete update,
+    constraint member_id_fkey foreign key (member_id) references public.profiles (id) on delete cascade,
     constraint org_id_fkey foreign key (org_id) references public.organizations (id) on delete cascade,
     constraint team_id_fkey foreign key (team_id) references public.teams (id) on delete cascade,
     constraint team_members_pkey primary key (member_id, org_id, team_id)
@@ -20,6 +20,7 @@ create table if not exists public.team_members (
 
 create table if not exists public.projects (
     id serial not null,
+    org_id uuid not null,
     name text not null,
     description text not null,
     avatar text,
