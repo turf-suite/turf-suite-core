595ee1a7764f44a3865830d465c3d19e39c928e5
Ryan Monahan <ryanaldo34@gmail.com>
2024-06-02 17:52:34 -0500

    finish basic task card display

--- stik/src/features/tasks/components/task-display.tsx
+++ stik/src/features/tasks/components/task-display.tsx
@@ -3,14 +3,22 @@ import {
   Box,
   Progress,
   VStack,
+  Flex,
+  ButtonGroup,
   Tag,
+  TagLeftIcon,
   TagLabel,
   Text,
   Avatar,
   Spacer,
   IconButton,
 } from '@chakra-ui/react';
-import { EditIcon } from '@chakra-ui/icons';
+import {
+  EditIcon,
+  CalendarIcon,
+  LinkIcon,
+  AttachmentIcon,
+} from '@chakra-ui/icons';
 
 interface TaskDisplayProps {
   name: string;
@@ -30,10 +38,11 @@ const prettyDate = (date: Date): string => {
 };
 
 const Task: React.FC<TaskDisplayProps> = (props: TaskDisplayProps) => (
-  <Box>
-    <VStack spacing="24px">
-      <Spacer>
-        <Tag size="sm" variant="subtle">
+  <Box maxWidth="250px" bg="primary.50">
+    <Flex align={'start'} flexDirection="column" gap="24px" grow={0}>
+      <Flex width="100%">
+        <Tag colorScheme="primary" size="sm">
+          <TagLeftIcon as={CalendarIcon} />
           {props.startDate !== undefined ? (
             <TagLabel>{`${prettyDate(props.startDate)} - ${prettyDate(
               props.dueDate
@@ -42,31 +51,59 @@ const Task: React.FC<TaskDisplayProps> = (props: TaskDisplayProps) => (
             <TagLabel>{prettyDate(props.dueDate)}</TagLabel>
           )}
         </Tag>
-        <IconButton aria-label="Edit Task" icon={<EditIcon />} />
-      </Spacer>
-      <VStack spacing="12px">
-        <Text variant="md">{props.id}</Text>
-        <Text variant="sm">{props.name}</Text>
-        <Avatar src={props.assigneeUrl} />
-      </VStack>
-      <VStack spacing="12px">
+        <Spacer />
+        <ButtonGroup>
+          <IconButton
+            color="primary"
+            size="sm"
+            aria-label="Edit Task"
+            icon={<EditIcon />}
+          />
+        </ButtonGroup>
+      </Flex>
+      <Flex direction="column" gap="12px">
+        <Text fontSize="md" fontWeight="bold">
+          {props.id}
+        </Text>
+        <Text fontSize="sm">{props.name}</Text>
+        <Flex width="100%">
+          <Avatar src={props.assigneeUrl} size="xs" />
+          <Spacer />
+          <ButtonGroup>
+            <IconButton
+              size="sm"
+              variant="solid"
+              aria-label="Task Attachments"
+              icon={<AttachmentIcon />}
+            />
+            <IconButton
+              variant="solid"
+              size="sm"
+              aria-label="Linked Tasks"
+              icon={<LinkIcon />}
+            />
+          </ButtonGroup>
+        </Flex>
+      </Flex>
+      <Flex direction="column" gap="12px" width="100%">
         <Progress
           size="sm"
+          colorScheme="accent"
           value={
             props.hoursCompleted > 0
               ? Math.round((props.hoursCompleted / props.hoursEstimated) * 100)
               : 0
           }
         />
-        <Text variant="sm">
+        <Text fontSize="xs">
           {props.hoursCompleted > 0
             ? `${Math.round(
               (props.hoursCompleted / props.hoursEstimated) * 100
             )}%`
             : '0%'}
         </Text>
-      </VStack>
-    </VStack>
+      </Flex>
+    </Flex>
   </Box>
 );
 export default Task;
