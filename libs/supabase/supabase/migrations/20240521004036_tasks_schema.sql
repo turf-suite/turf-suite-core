db793aec0c513ac09da52d3ee3b64ef58bcf414e
Ryan Monahan <ryanaldo34@gmail.com>
2024-06-02 23:19:30 -0500

    schema update + draggable task

--- libs/supabase/supabase/migrations/20240521004036_tasks_schema.sql
+++ libs/supabase/supabase/migrations/20240521004036_tasks_schema.sql
@@ -1,17 +1,21 @@
 create table public.tasks (
-  task_id numeric,
+  task_id text,
   project_id serial,
   board_name text,
   task_title text,
+  state_name text,
   task_description text,
   assignee uuid,
   reviewer uuid,
   due_date date,
+  date_started date,
   hours_estimated integer,
   hours_completed integer,
   constraint tasks_pkey primary key (task_id, project_id, board_name),
   constraint board_fkey foreign key (board_name, project_id)
     references public.boards (board_name, project_id) on delete cascade,
+  constraint board_state_fkey foreign key (project_id, board_name, state_name)
+    references public.board_states (project_id, board_name, name),
   constraint assignee_fkey foreign key (assignee)
     references public.profiles (user_id),
   constraint reviewer_fkey foreign key (reviewer)
@@ -21,10 +25,10 @@ create table public.tasks (
 create table public.parent_child_tasks (
   parent_task_project_id serial,
   parent_task_board_name text,
-  parent_task_id numeric,
+  parent_task_id text,
   child_task_project_id serial,
   child_task_board_name text,
-  child_task_id numeric,
+  child_task_id text,
   constraint parent_task_fkey foreign key (parent_task_id, parent_task_board_name, parent_task_project_id)
     references public.tasks (task_id, board_name, project_id) on delete cascade,
   constraint child_task_fkey foreign key (child_task_id, child_task_board_name, child_task_project_id)
@@ -35,8 +39,8 @@ create table public.parent_child_tasks (
 create table public.related_tasks (
   linked_task_project_id serial,
   linked_task_board_name text,
-  linked_task_id numeric,
-  task_id numeric,
+  linked_task_id text,
+  task_id text,
   task_project_id serial,
   task_board_name text,
   constraint task_fkey foreign key (task_id, task_board_name, task_project_id)
@@ -48,8 +52,8 @@ create table public.related_tasks (
 create table public.blocker_tasks (
   blocker_task_project_id serial,
   blocker_task_board_name text,
-  blocker_task_id numeric,
-  task_id numeric,
+  blocker_task_id text,
+  task_id text,
   task_project_id serial,
   task_board_name text,
   constraint task_fkey foreign key (task_id, task_board_name, task_project_id)
@@ -60,7 +64,7 @@ create table public.blocker_tasks (
 
 create table public.task_tags (
   tag_name text not null,
-  task_id numeric not null,
+  task_id text not null,
   board_name text not null,
   project_id serial not null,
   constraint task_fkey foreign key (task_id, board_name, project_id)
--- stik/src/features/tasks/components/task-display.tsx
+++ stik/src/features/tasks/components/task-display.tsx
@@ -12,13 +12,17 @@ import {
   Avatar,
   Spacer,
   IconButton,
+  list,
 } from '@chakra-ui/react';
 import {
   EditIcon,
   CalendarIcon,
   LinkIcon,
   AttachmentIcon,
+  DragHandleIcon,
 } from '@chakra-ui/icons';
+import { useDraggable } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
 
 interface TaskDisplayProps {
   name: string;
@@ -37,73 +41,106 @@ const prettyDate = (date: Date): string => {
   });
 };
 
-const Task: React.FC<TaskDisplayProps> = (props: TaskDisplayProps) => (
-  <Box maxWidth="250px" bg="primary.50">
-    <Flex align={'start'} flexDirection="column" gap="24px" grow={0}>
-      <Flex width="100%">
-        <Tag colorScheme="primary" size="sm">
-          <TagLeftIcon as={CalendarIcon} />
-          {props.startDate !== undefined ? (
-            <TagLabel>{`${prettyDate(props.startDate)} - ${prettyDate(
-              props.dueDate
-            )}`}</TagLabel>
-          ) : (
-            <TagLabel>{prettyDate(props.dueDate)}</TagLabel>
-          )}
-        </Tag>
-        <Spacer />
-        <ButtonGroup>
-          <IconButton
-            color="primary"
-            size="sm"
-            aria-label="Edit Task"
-            icon={<EditIcon />}
-          />
-        </ButtonGroup>
-      </Flex>
-      <Flex direction="column" gap="12px">
-        <Text fontSize="md" fontWeight="bold">
-          {props.id}
-        </Text>
-        <Text fontSize="sm">{props.name}</Text>
-        <Flex width="100%">
-          <Avatar src={props.assigneeUrl} size="xs" />
-          <Spacer />
-          <ButtonGroup>
-            <IconButton
+const Task: React.FC<TaskDisplayProps> = (props: TaskDisplayProps) => {
+  const { setNodeRef, attributes, listeners, isDragging, transform } =
+    useDraggable({
+      id: props.id,
+    });
+  return (
+    <Box
+      ref={setNodeRef}
+      maxWidth="275px"
+      bg="white"
+      padding="15px"
+      zIndex={isDragging ? '100' : 'auto'}
+      opacity={isDragging ? 0.3 : 1}
+      borderRadius="4px"
+      transform={CSS.Translate.toString(transform)}
+    >
+      <Flex width="100%" direction="column" gap="8px" align="center">
+        <IconButton
+          {...attributes}
+          {...listeners}
+          size="md"
+          variant="ghost"
+          aria-label="Drag Task"
+          icon={<DragHandleIcon sx={{ transform: 'rotate(90deg)' }} />}
+        />
+        <Flex
+          flex="1"
+          align={'start'}
+          flexDirection="column"
+          gap="24px"
+          grow={0}
+        >
+          <Flex width="100%">
+            <Tag colorScheme="primary" size="sm">
+              <TagLeftIcon as={CalendarIcon} />
+              {props.startDate !== undefined ? (
+                <TagLabel>{`${prettyDate(props.startDate)} - ${prettyDate(
+                  props.dueDate
+                )}`}</TagLabel>
+              ) : (
+                <TagLabel>{prettyDate(props.dueDate)}</TagLabel>
+              )}
+            </Tag>
+            <Spacer />
+            <ButtonGroup>
+              <IconButton
+                color="primary"
+                size="sm"
+                aria-label="Edit Task"
+                icon={<EditIcon />}
+              />
+            </ButtonGroup>
+          </Flex>
+          <Flex width="100%" direction="column" gap="12px">
+            <Text fontSize="md" fontWeight="bold">
+              {props.id}
+            </Text>
+            <Text fontSize="sm">{props.name}</Text>
+            <Flex width="100%">
+              <Avatar src={props.assigneeUrl} size="xs" />
+              <Spacer />
+              <ButtonGroup>
+                <IconButton
+                  size="sm"
+                  variant="solid"
+                  aria-label="Task Attachments"
+                  icon={<AttachmentIcon />}
+                />
+                <IconButton
+                  variant="solid"
+                  size="sm"
+                  aria-label="Linked Tasks"
+                  icon={<LinkIcon />}
+                />
+              </ButtonGroup>
+            </Flex>
+          </Flex>
+          <Flex direction="column" gap="12px" width="100%">
+            <Progress
               size="sm"
-              variant="solid"
-              aria-label="Task Attachments"
-              icon={<AttachmentIcon />}
+              colorScheme="accent"
+              value={
+                props.hoursCompleted > 0
+                  ? Math.round(
+                    (props.hoursCompleted / props.hoursEstimated) * 100
+                  )
+                  : 0
+              }
             />
-            <IconButton
-              variant="solid"
-              size="sm"
-              aria-label="Linked Tasks"
-              icon={<LinkIcon />}
-            />
-          </ButtonGroup>
+            <Text fontSize="xs">
+              {props.hoursCompleted > 0
+                ? `${Math.round(
+                  (props.hoursCompleted / props.hoursEstimated) * 100
+                )}%`
+                : '0%'}
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
-      <Flex direction="column" gap="12px" width="100%">
-        <Progress
-          size="sm"
-          colorScheme="accent"
-          value={
-            props.hoursCompleted > 0
-              ? Math.round((props.hoursCompleted / props.hoursEstimated) * 100)
-              : 0
-          }
-        />
-        <Text fontSize="xs">
-          {props.hoursCompleted > 0
-            ? `${Math.round(
-              (props.hoursCompleted / props.hoursEstimated) * 100
-            )}%`
-            : '0%'}
-        </Text>
-      </Flex>
-    </Flex>
-  </Box>
-);
+    </Box>
+  );
+};
 export default Task;
